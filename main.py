af0017003cba49b4dcc04c554145dfdcbec8e5dd
Amsky777 <adrianminklej@gmail.com>
2025-04-11 09:33:39 +0200

    WIP: simple data structure upgrade

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-users:list = [
-    {"name":"Adrian","location":"Toruń","posts":400},
-    {"name":"Krzysztof","location":"Baiłobrzegi","posts":500},
-    {"name":"Adrian","location":"Świecie","posts":300},
-    {"name":"Zuzia","location":"Radzyń_Podlaski","posts":700},
-]
+from utils.model import users
+from utils.controller import get_user_info
+
+
+def main():
+    get_user_info(users)
 
 
 
-def get_user_info(users_data:list)->None:
-
-    for user in users_data:
-        print(f"Twój znajomy {user['name']}! z miejscowości{user["location"]} opublikował {user['posts']} postów")
-
-
-get_user_info(users)
+if __name__ == "__main__":
+    main()
