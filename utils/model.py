d832f29a2e1af783adacd15c7af48f1921092986
Amsky777 <adrianminklej@gmail.com>
2025-04-16 11:23:14 +0200

    WIP: new function: remove user

--- utils/model.py
+++ utils/model.py
@@ -1,6 +1,6 @@
 users:list = [
     {"name":"Adrian","location":"Toruń","posts":400},
     {"name":"Krzysztof","location":"Baiłobrzegi","posts":500},
-    {"name":"Adrian","location":"Świecie","posts":300},
+    {"name":"Maja","location":"Świecie","posts":300},
     {"name":"Zuzia","location":"Radzyń_Podlaski","posts":700},
 ]
