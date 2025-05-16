fbf52f9a8bf6eb977d65863580f7dbf48bec27cf
Amsky777 <adrianminklej@gmail.com>
2025-05-16 05:32:42 +0200

    WIP: new function: update user

--- notatnik1.py
+++ notatnik1.py
@@ -1,20 +1,17 @@
-users:list = [
-    {"name":"Zuzia","location":"Radzyń_Podlaski","posts":700},
+import folium
 
-]
-print(users)
+# mapa = folium.Map(location=[52.21, 21.0], zoom_start=6)
+# folium. Marker(location=[52.21, 21.0], popup=f"").add_to(mapa)
+# mapa.save('mapa.html')
+def ger_cordinates(city_name:str)->list:
+    import requests
+    from bs4 import BeautifulSoup
+    url=f"https://pl.wikipedia.org/wiki/Warszawa"
+    response=requests.get(url).text
+    response_html=BeautifulSoup(response,'html.parser')
+    latitude=float(response_html.select(".latitude")[1].text.replace(",","."))
+    longitude=float(response_html.select(".longitude")[1].text.replace(",","."))
+    print(latitude,longitude)
+    return [latitude,longitude]
 
-def update_user(users_data: list)->None:
-
-    user_name=input("podaj imię użytkownika którego dane chcesz zaktualizowac: ")
-    for user in users_data:
-        if user["name"] == user_name:
-            user["name"] = input("podaj imię użytkownika: ")
-            user["location"] = input("podaj nową lokalizację użytkownika: ")
-            user["posts"] = int(input("podaj nową liczbę postów użytkownika:: "))
-
-update_user(users)
-
-
-
-
+ger_cordinates("Wałbrzych")
